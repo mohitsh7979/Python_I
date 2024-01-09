6fe8bd1611a46694e5bf1531dfe8efe9bb29a3d4
mohitsh7979 <mohitsh7979@gmail.com>
2024-01-09 18:32:17 +0530

    added

---
+++ index.py
@@ -0,0 +1,64 @@
+# variables and Data types
+
+# a = 10
+# b = 20
+# c = "mohit"
+# d = 1.5
+# e = True
+# print(a)
+# print(type(a))
+# print(b)
+# print(type(b))
+# print(c)
+# print(type(c))
+# print(d)
+# print(type(d))
+# print(e)
+# print(type(e))
+
+# name = 5
+
+# full name = 10 # ki hm asa variable ka name le skte beacuse esme white space aagyi h 
+
+# 500name = 50   # we can start our variable name from digit
+
+# 500 = 900   # we can not take our variable name from digit
+
+# we can use underscore when i declare our variable
+
+# a = 10
+# A = 20
+# print(a)
+
+# nameFullName = 50
+
+# FullName = 50
+# my_full_name = 80
+
+
+
+# name,age,location = "mohit",52,"jaipur"
+# print(name)
+# print(age)
+# print(location)
+
+
+# name = age = location = 56
+# print(name)
+# print(age)
+# print(location)
+
+
+# name = "mohit"
+# age = 56
+
+# print(name)
+# print(age)
+# 500name = 89
+
+
+
+
+
+
+
